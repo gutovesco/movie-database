e0f7d6f2fe0cb8a97ca02cb090bd8d15b0c56494
gutovesco <guto.vesco@hotmail.com>
2020-01-28 19:38:59 -0300

    style changes

---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Search (){
+    return(
+        <section>
+            <input type="text" placeholder="Pesquise por algo" className="searchbox"></input>
+        </section>
+    )
+}
+export default Search;
